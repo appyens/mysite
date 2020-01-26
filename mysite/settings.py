4c56c754a7e760b689a1717fa7d50fcc491b31da
appyens <an.anurag@msn.com>
2020-01-26 23:10:44 +0530

    settings.py changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,8 +131,8 @@ STATIC_URL = '/static/'
 # Email server configuration
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'an.anuraag@gmail.com'
-EMAIL_HOST_PASSWORD = '3UnTy!3###3'
+EMAIL_HOST_USER = 'email here'
+EMAIL_HOST_PASSWORD = 'password here'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
